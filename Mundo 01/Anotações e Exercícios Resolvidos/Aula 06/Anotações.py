ef052fc62600f90d40fb82c33ce10005d1b10d43
Leandro Felix <leandro.almeida3029@gmail.com>
2022-07-05 20:10:51 -0300

    update mundo 01

---
+++ Mundo 01/Anotações e Exercícios Resolvidos/Aula 06/Anotações.py
@@ -0,0 +1,21 @@
+#Tipos Primitivos
+
+#Quatro tipos primitivos básicos em Python são:
+#int, float, bool e str
+
+n1 = str(input('Digite um número: '))
+print(type(n1))
+print(n1.isnumeric())
+#n1 é uma string
+
+n2 = int(input('Digite outro número: '))
+print(type(n2))
+#n2 é um inteiro
+
+n3 = float(input('Digite outro número: '))
+print(type(n3))
+#n4 é um ponto flutuante
+
+n4 = bool(input('Digite outro número: '))
+print(type(n4))
+#n4 é um Booleano
