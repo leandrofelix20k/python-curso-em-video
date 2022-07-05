a4415bb14cf10c0d8fddf7e46847fd64d6c532a2
Leandro Felix <leandro.almeida3029@gmail.com>
2022-07-05 19:24:17 -0300

    Aula 04 ok

---
+++ Mundo 01/Anotações e Exercícios Resolvidos/Aula 04/Anotações.py
@@ -0,0 +1,15 @@
+#Priorizar o uso de aspas simples ('') para exibir mensagens, ao invés de aspas duplas
+
+print('Olá, Mundo!')
+
+#O uso do operador '+' e da vírgula pode variar dependendo do caso
+
+#print('Olá' + 5) -error
+print('Olá' , 5)
+
+#Toda váriavel é um objeto em python
+
+nome = input('Qual é o seu nome? ')
+idade = int(input('Qual é a sua idade? '))
+
+print ('Nome:', nome ,'\nIdade:' , idade)
---
+++ Mundo 01/Anotações e Exercícios Resolvidos/Aula 04/Desafio 01.py
@@ -0,0 +1,7 @@
+#Desafio 01:
+
+#crie um script Python que leia o nome de uma e mostre uma mensagem de 
+#boas-vindas de acordo com o valor digitado.
+
+nome = input('Digite o seu nome: ')
+print('Seja bem-vindo(a) ' + nome)
