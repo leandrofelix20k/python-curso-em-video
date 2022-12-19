c408040b456b3e9723d392bddbd3e07dc29f9939
Leandro Felix <leandro.almeida3029@gmail.com>
2022-12-19 11:45:37 -0300

    update Ex071

--- Mundo-02/Exercicios/Ex071.py
+++ Mundo-02/Exercicios/Ex071.py
@@ -126,4 +126,23 @@ for x in range(0, 4):
         print(f"{(valor - valorrestante) // cedulas[x]} cédulas de {cedulas[x]} reais")
 
         valorrestante = (valor // cedulas[x]) * cedulas[x]
+--------------------------------------------------------------------
+n = int(input('\033[36mQue valor você quer sacar? R$').strip())
+c50 = n//50
+c20 = (n % 50)//20
+c10 = ((n % 50) % 20)//10
+c5 = (((n % 50) % 20) % 10)//5
+c = (((n % 50) % 20) % 10) % 5
+if c50 != 0:
+    print(f'\033[31mTotal de {c50} cédulas de R$50')
+if c20 != 0:
+    print(f'\033[31mTotal de {c20} cédulas de R$20')
+if c10 != 0:
+    print(f'\033[31mTotal de {c10} cédulas de R$10')
+if c5 != 0:
+    print(f'\033[31mTotal de {c5} cédulas de R$5')
+if c != 0:
+    print(f'\033[31mTotal de {c} cédulas de R$1')
+print('\033[33m-=-\033[m'*21)
+print('\033[35mVolte sempre ao BANCO CEV! Tenha um bom dia!')
 '''
